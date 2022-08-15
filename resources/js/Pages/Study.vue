d0e24d8496028599307f09cb7d3d415430cb3aaf
jin1125 <shogo.jinta@gmail.com>
2022-08-15 15:01:51 +0900

    [fix]Studyページのリファクタリング

--- resources/js/Pages/Study.vue
+++ resources/js/Pages/Study.vue
@@ -4,17 +4,15 @@ import Page from '@/Layouts/Page.vue';
 
 <template>
   <Page >
-    <div class="text-blue">
-      <div class="flex items-center my-16 mx-20 space-x-5">
-        <img
-          class="w-40"
-          src="image/study.png"
-          alt="profile image"
-        >
-        <h2 class="font-bold text-2xl">
-          Study record
-        </h2>
-      </div>
+    <div class="flex items-center my-16 mx-20 space-x-5">
+      <img
+        class="w-40"
+        src="image/study.png"
+        alt="profile image"
+      >
+      <h2 class="font-bold text-3xl">
+        Study record
+      </h2>
     </div>
   </Page>
 </template>
