2fcfbcb553c55f1888214eddcc72308bd6fd1b90
jin1125 <shogo.jinta@gmail.com>
2022-08-17 10:53:29 +0900

    [fix]ルートを変更

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StudyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::inertia('/', 'Profile')->name('profile');
+Route::inertia('/', 'Profile')
+    ->name('profile');
 
-Route::inertia('/study', 'Study')->name('study');
+Route::prefix('study')
+    ->controller(StudyController::class)
+    ->name('study')
+    ->group(function () {
+    Route::get('/', 'showStudy');
+});
 
 require __DIR__.'/auth.php';
