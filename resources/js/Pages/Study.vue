c862df8f848cfbd34fe84e6c4b71638026e43db0
jin1125 <shogo.jinta@gmail.com>
2022-08-18 23:52:21 +0900

    [add]ログイン後のモーダルデザインを作成

--- resources/js/Pages/Study.vue
+++ resources/js/Pages/Study.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import Modal from '@/Components/Modal.vue';
 import Page from '@/Layouts/Page.vue';
 import { useForm } from '@inertiajs/inertia-vue3';
+import { Inertia } from '@inertiajs/inertia';
 
 const isOpenModal = ref(false);
 
@@ -14,17 +15,23 @@ defineProps({
 })
 
 const form = useForm({
-    email: '',
-    password: '',
+  email: '',
+  password: '',
 });
 
 const onOpenModalClick = () => isOpenModal.value = true;
 
 const onLoginClick = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
-        onSuccess: () => isOpenModal.value = false,
-    });
+  form.post(route('login'), {
+    onFinish: () => form.reset('password'),
+    onSuccess: () => isOpenModal.value = false,
+  });
+};
+
+const onLogoutClick = () => {
+  Inertia.post(route('logout'))
+
+  isOpenModal.value = false
 };
 </script>
 
@@ -60,75 +67,106 @@ const onLoginClick = () => {
     </div>
 
     <Modal v-model="isOpenModal">
-      <div class="space-y-10 text-blue text-center">
-        <div class="font-bold text-blue text-2xl">
-          Admin login
+      <div class="text-blue text-center">
+        <div class="font-bold mb-10 text-blue text-2xl">
+          {{ isLogin ? 'Edit?' : 'Admin login' }}
         </div>
 
-        <div class="space-y-5">
-          <div>
-            <div class="gap-5 grid grid-cols-4 items-center text-start">
-              <label
-                class="col-span-1 font-bold"
-                for="email"
-              >
-                Email
-              </label>
-              <div class="border-b border-blue col-span-3">
-                <input
-                  class="appearance-none border-none py-1 w-full focus:outline-none"
-                  id="email"
-                  type="email"
-                  v-model="form.email"
-                  autocomplete="on"
-                  autofocus
-                  required
+        <div v-if="!isLogin">
+          <div class="mb-10 space-y-5">
+            <div>
+              <div class="gap-5 grid grid-cols-4 items-center text-start">
+                <label
+                  class="col-span-1 font-bold"
+                  for="email"
                 >
+                  Email
+                </label>
+                <div class="border-b border-blue col-span-3">
+                  <input
+                    class="appearance-none border-none py-1 w-full focus:outline-none"
+                    id="email"
+                    type="email"
+                    v-model="form.email"
+                    autocomplete="on"
+                    autofocus
+                    required
+                  >
+                </div>
               </div>
+              <p
+                v-if="form.errors.email"
+                class="text-sm text-red-600"
+              >
+                {{ form.errors.email }}
+              </p>
             </div>
-            <p
-              v-if="form.errors.email"
-              class="text-sm text-red-600"
-            >
-              {{ form.errors.email }}
-            </p>
+
+            <div>
+              <div class="gap-5 grid grid-cols-4 items-center text-start">
+                <label
+                  class="col-span-1 font-bold"
+                  for="password"
+                >
+                  Password
+                </label>
+                <div class="border-b border-blue col-span-3">
+                  <input
+                    class="appearance-none border-none py-1 w-full focus:outline-none"
+                    id="password"
+                    type="password"
+                    v-model="form.password"
+                    autocomplete="on"
+                    required
+                  >
+                </div>
+              </div>
+              <p
+                v-if="form.errors.password"
+                class="text-red-600 text-sm"
+              >
+                {{ form.errors.password }}
+              </p>
+            </div>
           </div>
 
-          <div>
-            <div class="gap-5 grid grid-cols-4 items-center text-start">
-              <label
-                class="col-span-1 font-bold"
-                for="password"
-              >
-                Password
-              </label>
-              <div class="border-b border-blue col-span-3">
-                <input
-                  class="appearance-none border-none py-1 w-full focus:outline-none"
-                  id="password"
-                  type="password"
-                  v-model="form.password"
-                  autocomplete="on"
-                  required
-                >
-              </div>
-            </div>
-            <p
-              v-if="form.errors.password"
-              class="text-red-600 text-sm"
-            >
-              {{ form.errors.password }}
-            </p>
-          </div>
+          <button
+            @click.prevent="onLoginClick()"
+            class="font-bold bg-blue py-1 rounded-full
+              text-white w-28 hover:opacity-80"
+          >
+            Login
+          </button>
         </div>
 
-        <button
-          @click.prevent="onLoginClick()"
-          class="font-bold bg-blue py-1 px-6 rounded-full
-            text-white hover:opacity-80"
+        <div
+          v-else
+          class="flex-col space-y-5 w-52"
         >
-          Login
-        </button>
+          <button
+            class="font-bold bg-blue block mx-auto py-1 rounded-full
+              text-white w-28 hover:opacity-80"
+          >
+            OK
+          </button>
+
+          <button
+            class="font-bold bg-white block border border-blue mx-auto
+              py-1 rounded-full text-blue w-28 hover:opacity-80"
+          >
+            Cancel
+          </button>
+
+          <div class="text-end">
+            <p
+              @click.prevent="onLogoutClick()"
+              class="cursor-pointer inline-block select-none
+                underline hover:opacity-80"
+            >
+              Logout
+            </p>
+          </div>
+        </div>
       </div>
     </Modal>
   </Page>
