bad1d14cdc889d0e783b1cc6030fd798631fd300
jin1125 <shogo.jinta@gmail.com>
2022-08-21 16:35:58 +0900

    [add]新規投稿リクエスト処理を実装

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Study;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
@@ -19,5 +20,22 @@ class StudyController extends Controller
             'isLogin' => Auth::check(),
             'studyRecords' => $studyRecords
         ]);
+    }
+
+    /**
+     * 新規投稿リクエストを送信
+     */
+    public function sendNewPost(Request $request)
+    {
+        Study::create([
+            'title'       => $request->input('title'),
+            'link'        => $request->input('link'),
+            'progress'    => $request->input('progress'),
+            'complete_at' => $request->input('complete_at'),
+            'comment'     => $request->input('comment'),
+            'category'    => $request->input('category'),
+        ]);
+
+        return redirect()->route('study');
     }
 }
