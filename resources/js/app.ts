b246b5153a34f9e8fa5c3d9d0ad3c021a4009714
jinta <shogo.jinta@qnote.co.jp>
2022-09-07 16:00:27 +0900

    [fix]リファクタリング

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -7,7 +7,7 @@ import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import {
   faAddressCard,
   faBookOpen,
